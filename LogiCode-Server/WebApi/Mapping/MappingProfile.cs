24f8c42538e6db3e59dd715d3b78919b0b890570
leah <l0583280864@gmail>
2025-05-13 15:37:55 +0300

    mapping

--- LogiCode-Server/WebApi/Mapping/MappingProfile.cs
+++ LogiCode-Server/WebApi/Mapping/MappingProfile.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Core;
+using Core.DTO;
 
 namespace Core.Mapping
 {
@@ -13,19 +14,14 @@ namespace Core.Mapping
         public MappingProfile()
         {
             CreateMap<DAO.User, DTO.User>().ReverseMap();
-            CreateMap<DAO.Course, DTO.Course>().ForMember(dest => dest.LessonIds, opt => opt.MapFrom(src => src.Lessons.Select(l => l.Id)));
-            CreateMap<DTO.Course, DAO.Course>().ForMember(dest => dest.Lessons, opt => opt.Ignore()); 
+            CreateMap<DAO.Course, DTO.Course>().ReverseMap();
             CreateMap<DAO.Exercise,DTO.Exercise>().ReverseMap();
-            CreateMap<DAO.Lesson, DAO.Lesson>().ReverseMap(); 
-            CreateMap<DAO.Payment, DAO.Payment>().ReverseMap();
-            CreateMap<DAO.StudentCourse,DAO.StudentCourse>().ReverseMap();
-            CreateMap<DAO.StudentLesson,DAO.StudentLesson>().ReverseMap();
-            CreateMap<DAO.Teacher,DTO.Teacher>().ForMember(dest => dest.CourseIds, opt => opt.MapFrom(src => src.Courses.Select(l => l.Id)));
-            CreateMap<DTO.Teacher, DAO.Teacher>().ForMember(dest => dest.Courses, opt => opt.Ignore());
-            CreateMap<DAO.Student, DTO.Student>().ForMember(dest => dest.StudentLessonIds, opt => opt.MapFrom(src => src.StudentLessons.Select(l => l.Id)));
-            CreateMap<DTO.Student, DAO.Student>().ForMember(dest => dest.StudentLessons, opt => opt.Ignore());
-            CreateMap<DAO.Student, DTO.Student>().ForMember(dest => dest.StudentCourseIds, opt => opt.MapFrom(src => src.StudentCourses.Select(l => l.Id)));
-            CreateMap<DTO.Student, DAO.Student>().ForMember(dest => dest.StudentCourses, opt => opt.Ignore());
+            CreateMap<DAO.Lesson, DTO.Lesson>().ReverseMap(); 
+            CreateMap<DAO.Payment, DTO.Payment>().ReverseMap();
+            CreateMap<DAO.StudentCourse,DTO.StudentCourse>().ReverseMap();
+            CreateMap<DAO.StudentLesson, DTO.StudentLesson>().ReverseMap();
+            CreateMap<DAO.Teacher, DTO.Teacher>().ReverseMap();
+            CreateMap<DAO.Student, DTO.Student>().ReverseMap();
         }
     }
 }
